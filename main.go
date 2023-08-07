7cdd8710c8fd61567cbb9de97921e47322ff783e
DmitriyLewen <dmitriy.lewen@smartforce.io>
2023-08-07 13:19:57 +0600

    init commit

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"github.com/xeipuuv/gojsonschema"
+)
+
+func main() {
+	schemaLoader := gojsonschema.NewReferenceLoader("file://./schemas/bom-1.5.schema.json")
+	documentLoader := gojsonschema.NewReferenceLoader("file://./ubuntu2204.cdx.json")
+
+	result, err := gojsonschema.Validate(schemaLoader, documentLoader)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	if result.Valid() {
+		fmt.Printf("The document is valid\n")
+	} else {
+		fmt.Printf("The document is not valid. see errors :\n")
+		for _, desc := range result.Errors() {
+			fmt.Printf("- %s\n", desc)
+		}
+	}
+}
